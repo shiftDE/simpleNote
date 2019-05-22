55c7a5030c1f4b8acfbb38ef49542046debdba6a
shiftDE <45703781+shiftDE@users.noreply.github.com>
2019-05-22 16:19:13 +0800

    menu router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListComponent } from './components/list/list.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AccountsService } from './services/accounts.service';
+import { LoginComponent } from './components/login/login.component';
+import { AboutComponent } from './components/about/about.component';
+
+const pathRouters: Routes = [
+  {path: '', redirectTo: 'HomeComponent', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
@@ -15,11 +24,13 @@ import { AccountsService } from './services/accounts.service';
     HeaderComponent,
     HomeComponent,
     ListComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(pathRouters)
   ],
   providers: [AccountsService],
   bootstrap: [AppComponent]
